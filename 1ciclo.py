e265262005df429f8dcf8de9649b139ad45a5519
lucas <lucas.gbarreto@b2wdigital.com>
2022-03-23 14:13:52 -0300

    add input das temperaturas

--- 1ciclo.py
+++ 1ciclo.py
@@ -48,22 +48,7 @@ def CicloCompressaoDeVaporComTemperaturas (fluido, vazao_refrigerante,t_superA='
     ciclo.Exibir('h','p','s')
     print(ciclo.T[1])
     
-    # pp = PropertyPlot('HEOS::R134a', 'PH', unit_system='EUR')
-    # pp.calc_isolines(CoolProp.iQ, num=11)
-    # cycle = SimpleCompressionCycle('HEOS::R134a', 'PH', unit_system='EUR')
-    # T0 = t_evap 
-    # pp.state.update(CoolProp.QT_INPUTS,0.0,T0-10)
-    # p0 = pp.state.keyed_output(CoolProp.iP)
-    # T2 = t_cond  
-    # pp.state.update(CoolProp.QT_INPUTS,1.0,T2+15)
-    # p2 = pp.state.keyed_output(CoolProp.iP)
-    # pp.calc_isolines(CoolProp.iT, [T0-273.15,T2-273.15], num=2)
-    # cycle.simple_solve(T0, p0, T2, p2, 0.7,'HEOS::R134a', SI=True)
-    # cycle.steps = 50
-    # sc = cycle.get_state_changes()
-    # pp.draw_process(sc)    
-    # plt.close(cycle.figure)
-    # pp.show()
+    
     
 
 CicloCompressaoDeVaporComTemperaturas('R134a',0.03)
